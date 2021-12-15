b2101954f0a4c5e85bb23795f21ac1b2ddf8c100
AabhashRai13 <raiaabhash3@gmail.com>
2021-12-15 11:51:00 +0545

    response code added

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -13,3 +13,25 @@ enum DataSource {
   cacheError,
   noInterntConnection
 }
+
+class ResponseCode {
+  // API status codes
+  static const int success = 200; // success with data
+  static const int noContent = 201; // success with no content
+  static const int badRequest = 400; // failure, api rejected the request
+  static const int frobidden = 403; // failure, api rejected the request
+  static const int unauthorised = 401; // failure user is not authorised
+  static const int notFound =
+      404; // failure, api url is not correct and not found
+  static const int internalServerError =
+      500; // failure, crash happened in server side
+
+  // local status code
+  static const int unknown = -1;
+  static const int connectTimeout = -2;
+  static const int cancel = -3;
+  static const int receiveTimeout = -4;
+  static const int sendTimeout = -5;
+  static const int cacheError = -6;
+  static const int noInterntConnection = -7;
+}
