024f6aee65ed4a5210ec209797717725e02a4ece
AabhashRai13 <raiaabhash3@gmail.com>
2021-12-15 12:03:40 +0545

    data source extension done

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,8 +1,10 @@
+import 'failure.dart';
+
 enum DataSource {
   success,
   noContent,
   badRequest,
-  frobidden,
+  forbidden,
   unauthorised,
   notFound,
   internalServerError,
@@ -11,7 +13,7 @@ enum DataSource {
   receiveTimeout,
   sendTimeout,
   cacheError,
-  noInterntConnection
+  noInternetConnection
 }
 
 class ResponseCode {
@@ -19,7 +21,7 @@ class ResponseCode {
   static const int success = 200; // success with data
   static const int noContent = 201; // success with no content
   static const int badRequest = 400; // failure, api rejected the request
-  static const int frobidden = 403; // failure, api rejected the request
+  static const int forbidden = 403; // failure, api rejected the request
   static const int unauthorised = 401; // failure user is not authorised
   static const int notFound =
       404; // failure, api url is not correct and not found
@@ -33,5 +35,70 @@ class ResponseCode {
   static const int receiveTimeout = -4;
   static const int sendTimeout = -5;
   static const int cacheError = -6;
-  static const int noInterntConnection = -7;
+  static const int noInternetConnection = -7;
+}
+
+class ResponseMessage {
+  // API status codes
+  static const String success = "success"; // success with data
+  static const String noContent =
+      "success with no content"; // success with no content
+  static const String badRequest =
+      "Bad request, try again later"; // failure, api rejected the request
+  static const String forbidden =
+      "forbidden request, try again later"; // failure, api rejected the request
+  static const String unauthorised =
+      "user is unauthorised, try again later"; // failure user is not authorised
+  static const String notFound =
+      "Url is not found, try again later"; // failure, api url is not correct and not found
+  static const String internalServerError =
+      "some thing went wrong, try again later"; // failure, crash happened in server side
+
+  // local status code
+  static const String unknown = "some thing went wrong, try again later";
+  static const String connectTimeout = "time out error, try again later";
+  static const String cancel = "request was cancelled, try again later";
+  static const String receiveTimeout = "time out error, try again later";
+  static const String sendTimeout = "time out error, try again later";
+  static const String cacheError = "Cache error, try again later";
+  static const String noInternetConnection =
+      "Please check your internet connection";
+}
+
+extension DataSourceExtension on DataSource {
+  Failure getFailure() {
+    switch (this) {
+      case DataSource.badRequest:
+        return Failure(ResponseCode.badRequest, ResponseMessage.badRequest);
+      case DataSource.forbidden:
+        return Failure(ResponseCode.forbidden, ResponseMessage.forbidden);
+      case DataSource.unauthorised:
+        return Failure(ResponseCode.unauthorised, ResponseMessage.unauthorised);
+      case DataSource.notFound:
+        return Failure(ResponseCode.notFound, ResponseMessage.notFound);
+      case DataSource.internalServerError:
+        return Failure(ResponseCode.internalServerError,
+            ResponseMessage.internalServerError);
+      case DataSource.connectTimeout:
+        return Failure(
+            ResponseCode.connectTimeout, ResponseMessage.connectTimeout);
+      case DataSource.cancel:
+        return Failure(ResponseCode.cancel, ResponseMessage.cancel);
+      case DataSource.receiveTimeout:
+        return Failure(
+            ResponseCode.receiveTimeout, ResponseMessage.receiveTimeout);
+      case DataSource.sendTimeout:
+        return Failure(ResponseCode.sendTimeout, ResponseMessage.sendTimeout);
+      case DataSource.cacheError:
+        return Failure(ResponseCode.cacheError, ResponseMessage.cacheError);
+      case DataSource.noInternetConnection:
+        return Failure(ResponseCode.noInternetConnection,
+            ResponseMessage.noInternetConnection);
+      default:
+        return Failure(
+          ResponseCode.unknown,
+          ResponseMessage.unknown,
+        );
+    }
+  }
 }
