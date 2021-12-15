71e5641adb45555fc17f041a4b1d85546aca5dbd
AabhashRai13 <raiaabhash3@gmail.com>
2021-12-15 11:48:33 +0545

    error handler enum added

---
+++ lib/data/network/error_handler.dart
@@ -0,0 +1,15 @@
+enum DataSource {
+  success,
+  noContent,
+  badRequest,
+  frobidden,
+  unauthorised,
+  notFound,
+  internalServerError,
+  connectTimeout,
+  cancel,
+  receiveTimeout,
+  sendTimeout,
+  cacheError,
+  noInterntConnection
+}
